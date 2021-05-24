93cdede5748e067d2599c1292c89c6bf45a71fc5
Jesper Olofsson <46094983+Thottbot@users.noreply.github.com>
2021-05-24 11:26:52 +0200

    inital impl (#1)
    * inital impl
    
    * review fixes
    
    * updated README
    
    Co-authored-by: Jesper Olofsson <jesper.olofsson@indentive.se>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lynx-example-integration
-Example integration for exporting data from the Lynx platform
+Example integration for exporting data from the Lynx platform using the [go-lynx](https://github.com/IoTOpen/go-lynx, "go-lynx") library.
+
+This integration exports data from functions created by [Lynx-example-manager](https://github.com/IoTOpen/lynx-example-manager "Lynx-example-manager")
+and will not work without the proper devices and functions that it sets up. Also note that import and export should be done simultaneously inorder to see any sort of result.
+
+### Configure
+Create a new configuration file based on the [lynx-integration.example.yml](lynx-integration.example.yml) and name it `lynx-integration.yml` and place it in the root folder of the project.
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/IoTOpen/go-lynx"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var client *lynx.Client
+
+func configure() {
+	viper.SetConfigName("lynx-integration")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(".")
+
+	viper.SetDefault("api.base", "https://domain.tld")
+	viper.SetDefault("api.key", "secret")
+	viper.SetDefault("api.broker", "tcp://domain.tld:port")
+	viper.SetDefault("lynx.installation_id", 1)
+
+	if err := viper.ReadInConfig(); err != nil {
+		_ = viper.SafeWriteConfig()
+		log.Fatalln("Config:", err)
+	}
+}
+
+func lynxClientSetup() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(viper.GetString("api.broker"))
+	opts.SetCleanSession(true)
+	opts.SetClientID("lynx-integration-example")
+	opts.SetConnectTimeout(time.Second)
+
+	client = lynx.NewClient(&lynx.Options{
+		Authenticator: lynx.AuthApiKey{
+			Key: viper.GetString("api.key"),
+		},
+		ApiBase:     viper.GetString("api.base"),
+		MqttOptions: opts,
+	})
+
+	for err := client.MQTTConnect(); err != nil; err = client.MQTTConnect() {
+		log.Println("MQTT error connecting:", err)
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func subscribe(fn *lynx.Function) {
+	topic, _ := fn.Meta["topic_read"]
+	if token := client.Mqtt.Subscribe(topic, 2, messageHandler); token.WaitTimeout(time.Second) && token.Error() != nil {
+		log.Fatalf("MQTT: Could not subscribe to topic: %s, error: %s", topic, token.Error())
+	}
+}
+
+func messageHandler(_ mqtt.Client, message mqtt.Message) {
+	m := &lynx.Message{}
+	if err := json.Unmarshal(message.Payload(), m); err != nil {
+		log.Println("Failed to unmarshal payload:", err)
+		return
+	}
+	log.Printf("%s: %v", time.Unix(m.Timestamp, 0), m.Value)
+}
+
+func main() {
+	configure()
+	lynxClientSetup()
+
+	installationID := viper.GetInt64("lynx.installation_id")
+	devices, err := client.GetDevices(installationID, map[string]string{
+		"example.type": "go-lynx",
+	})
+	if err != nil {
+		log.Fatalln("Could not fetch devices:", err)
+	} else if len(devices) == 0 {
+		log.Fatal("No device found")
+	}
+	functions, err := client.GetFunctions(installationID, map[string]string{
+		"device_id":    fmt.Sprintf("%d", devices[0].ID),
+		"example.type": "go-lynx",
+	})
+	if err != nil {
+		log.Fatalln("Could not fetch function:", err)
+	}
+	subscribe(functions[0])
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	<-sigc
+}
